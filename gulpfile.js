1deaa34d01ff8dbf3d569eeb1e99d57805f9eeda
Brian Troncone <briantroncone@Brians-Mac-mini.local>
2015-02-22 22:47:25 -0500

    updated for flux

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,38 @@ var source = require('vinyl-source-stream');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 var port = process.env.port || 3000;
-
-gulp.task('sass', function() {
+var autoPrefixerOptions = {
+    broswers: ['last 2 version', '> %5']
+}
+gulp.task('sass', function () {
     gulp.src('./app/src/scss/**/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer(autoPrefixerOptions))
+        .pipe(concat('custom.css'))
         .pipe(gulp.dest('./app/dist'));
 });
 
-gulp.task('browserify', function() {
-    browserify('./app/src/js/app.js')
-    .transform('reactify', { 'es6': true })
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./app/dist'));
+gulp.task('css-thirdparty', function () {
+    gulp.src('.app/src/css-thirdparty/**/*.css')
+        .pipe(autoprefixer(autoPrefixerOptions))
+        .pipe(concat('css-thirdparty.css'))
+        .pipe(gulp.dest('./app/dist'))
 });
 
-gulp.task('connect', function() {
+gulp.task('browserify', function () {
+    browserify('./app/src/js/app.js')
+        .transform('reactify', {
+            'es6': true
+        })
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./app/dist'));
+});
+
+gulp.task('connect', function () {
     connect.server({
         root: 'app',
         port: port,
@@ -28,7 +43,7 @@ gulp.task('connect', function() {
     });
 });
 
-gulp.task('open', function(){
+gulp.task('open', function () {
     var options = {
         url: 'http://localhost:' + port,
     };
@@ -47,13 +62,14 @@ gulp.task('css', function () {
     gulp.src('./app/dist/**/*.css').pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('./app/index.html', ['html']);
     gulp.watch('./app/dist/**/*.js', ['js']);
     gulp.watch('./app/dist/**/*.css', ['css']);
+    gulp.watch('./app/src/**/*.css', ['css-thirdparty'])
     gulp.watch('./app/src/**/*.scss', ['sass']);
     gulp.watch('./app/src/**/*.js', ['browserify']);
 });
 
-gulp.task('default', ['browserify', 'sass']);
-gulp.task('serve', ['browserify', 'sass', 'connect', 'open', 'watch']);
+gulp.task('default', ['browserify', 'sass', 'css-thirdparty']);
+gulp.task('serve', ['browserify', 'sass', 'css-thirdparty', 'connect', 'open', 'watch']);
